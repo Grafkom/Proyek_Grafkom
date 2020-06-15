764fc924688d066a657ba2619ed1b2762e323376
Vinny <57027830+VinnyTan20@users.noreply.github.com>
2020-06-15 12:25:13 +0700

    tambahan

--- Proyek Grafkom/Casa3.0/Main.cs
+++ Proyek Grafkom/Casa3.0/Main.cs
@@ -33,7 +33,7 @@ namespace TareaGL
 			casa.Add(new Casa2());
 
 			casa.Add(new Librero(new Point3D(210,0,-318),0));
-			casa.Add(new Librero(new Point3D(210,Librero.Height+.3,-316),-3));
+			casa.Add(new Librero(new Point3D(210,Librero.Height+.3,-318),0));
 			casa.Add(new Lamp(new Point3D(60,270,190),70));
             casa.Add(new Lamp(new Point3D(300,270,350),70));
             casa.Add(new Lamp(new Point3D(350,270,-500),70));
@@ -95,20 +95,21 @@ namespace TareaGL
 			//casa.Add(new Silla(new Point3D(330,0,240),180));
 			//casa.Add(new Silla(new Point3D(385,0,110),90));
 			//casa.Add(new Silla(new Point3D(385,0,290),-90));
-			casa.Add(new Plato(new Point3D(380,obj.Height+.2,200)));
-			casa.Add(new Vaso(new Point3D(380,obj.Height+.2,230)));
+			casa.Add(new Plato(new Point3D(380,108.2,200)));
+			//casa.Add(new Vaso(new Point3D(380,obj.Height+.2,230)));
 			//casa.Add(new MesitaDeNoche(new Point3D(455,0,-140),-90));
 			//casa.Add(new MesitaDeNoche(new Point3D(455,0,-380),-90));
 			//casa.Add(new MesitaDeNoche(new Point3D(455,0,-580),-90));
 			//casa.Add(new MesitaDeNoche(new Point3D(-280,0,-340),90));
-			obj = new EstanteHorizontal(new Point3D(330,0,-70),180);
+			obj = new EstanteHorizontal(new Point3D(330,0,-40),180);
 			casa.Add(obj);
 
+			casa.Add(new Butaca(new Point3D(350, 0, -280), 0, 2));
 			//casa.Add(new Butaca(new Point3D(-30,0,200),90,2));
 			//casa.Add(new Butaca(new Point3D(200,0,270),250,1));
 			//casa.Add(new Butaca(new Point3D(200,0,110),-70,1));
 
-			casa.Add(new MesetaConFregadero(new Point3D(-307,0,-64),0));			
+			casa.Add(new teko(new Point3D(-307,0,-64),0));			
 
 			casa.Add(new Clock(new Point3D(485,200,325),270));
 			GlObject c = new TranslatedObject(new Point3D(0,0,-70),casa);
---
+++ Proyek Grafkom/Casa3.0/teko1.cs
@@ -0,0 +1,87 @@
+using System;
+using Tao.OpenGl;
+
+namespace TareaGL
+{
+	/// <summary>
+	/// Summary description for MesetaConFregadero.
+	/// </summary>
+	public class teko1:Plantilla
+	{
+
+		protected override void setInitialParams(object[] InitialParams) 
+		{
+			yInc=0;
+			height=100;
+			alto=height/100;
+		}
+		public teko1(Point3D center, double angle): base(center,angle)
+		{
+			this.customRendering=true;
+		}
+
+		double largo = 1.97 , alto, ancho = 0.54 , anchoF = 0.15 , anchoM = 0.05 ;
+		double lead=1.7;
+		
+		protected double distance=0;
+		public override void Prepare (Avatar observer) 
+		{
+			distance = (observer.Origin-this.center-new Point3D(largo*100/2,alto*100,-ancho*100/2)).Norm;
+		}
+
+		protected override void customRenderer()
+		{
+
+			Gl.glCallList(this.idObject);
+
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, GlUtils.Texture("AZULEJO"));
+			Gl.glColor3d(0.1, 0.15, .1);
+
+			double d = (int)this.distance / 50;
+			Gl.glBegin(Gl.GL_QUADS);
+
+			//pared
+
+			//Gl.glNormal3d(0,0,1);Gl.glTexCoord2d(0,0);Gl.glVertex3d(0,alto*100,-ancho*100+d);
+			//Gl.glNormal3d(0,0,1);Gl.glTexCoord2d(12,0);Gl.glVertex3d(largo*100,alto*100,-ancho*100+d);
+			//Gl.glNormal3d(0,0,1);Gl.glTexCoord2d(12,5);Gl.glVertex3d(largo*100,(alto+0.5)*100,-ancho*100+d);
+			//Gl.glNormal3d(0,0,1);Gl.glTexCoord2d(0,5);Gl.glVertex3d(0,(alto+0.5)*100,-ancho*100+d);
+
+			//pared derecha
+
+			//Gl.glNormal3d(-1,0,0);Gl.glTexCoord2d(0,0);Gl.glVertex3d(largo*100-d,alto*100,-ancho*100);
+			//Gl.glNormal3d(-1,0,0);Gl.glTexCoord2d(5,0);Gl.glVertex3d(largo*100-d,alto*100,0);
+			//Gl.glNormal3d(-1,0,0);Gl.glTexCoord2d(5,5);Gl.glVertex3d(largo*100-d,(alto+0.5)*100,0);
+			//Gl.glNormal3d(-1,0,0);Gl.glTexCoord2d(0,5);Gl.glVertex3d(largo*100-d,(alto+0.5)*100,-ancho*100);
+
+			//pared izquierda
+
+			//Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(0,5);Gl.glVertex3d(d,(alto+0.5)*100,-ancho*100);
+			//Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(5,5);Gl.glVertex3d(d,(alto+0.5)*100,0);
+			//Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(5,0);Gl.glVertex3d(d,alto*100,0);
+			//Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(0,0);Gl.glVertex3d(d,alto*100,-ancho*100);
+
+			//if (lead>0) 
+			//{
+			// Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(0,6);Gl.glVertex3d(d,(alto)*100,0);
+			// Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(lead*5,6);Gl.glVertex3d(d,(alto)*100,lead*100);
+			// Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(lead*5,0);Gl.glVertex3d(d,0,lead*100);
+			// Gl.glNormal3d(1,0,0);Gl.glTexCoord2d(0,0);Gl.glVertex3d(d,0,0);
+			//}
+
+			Gl.glEnd();
+		}
+		protected override void Particular() 
+	 	{
+			Gl.glColor3d(1, 1, 1);
+			Gl.glBindTexture(Gl.GL_TEXTURE_2D, GlUtils.Texture("old2"));
+			Gl.glPushMatrix();
+			double tph = 10;
+			Gl.glTranslated(largo*100 / 4, Height + tph*.8, -ancho * 100 / 2);
+			Gl.glRotated(30, 0, 1, 0);
+			Glut.glutSolidTeapot(10);
+			Gl.glPopMatrix();
+
+		}
+	}
+}
